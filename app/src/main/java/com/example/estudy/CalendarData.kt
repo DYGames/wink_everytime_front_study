c01345020f22d2bc5f3d7942bd6568a0da8800de
dygames <dygames@naver.com>
2021-10-30 12:36:27 +0900

    add 2nd week homework

--- app/src/main/java/com/example/estudy/CalendarData.kt
+++ app/src/main/java/com/example/estudy/CalendarData.kt
@@ -2,7 +2,7 @@ package com.example.estudy
 
 class CalendarData(_idx: Int, _title: String, _user: String, _location: String) {
     val idx = _idx
-    val title = _title
-    val user = _user
-    val location = _location
+    var title = _title
+    var user = _user
+    var location = _location
 }
--- app/src/main/java/com/example/estudy/MainActivity.kt
+++ app/src/main/java/com/example/estudy/MainActivity.kt
@@ -1,24 +1,32 @@
 package com.example.estudy
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.view.Gravity
-import android.view.View
-import android.view.ViewGroup
-import android.widget.GridLayout
-import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.get
+import android.view.*
+import android.widget.*
+
+import androidx.core.view.MenuItemCompat
+
 
 class MainActivity : AppCompatActivity() {
 
     val days = arrayOf("", "월", "화", "수", "목", "금", "토", "일")
     val times = Array(24, { i -> (i.toString() + "시") })
-    val calendarData: Map<Int, CalendarData> = mapOf(
+    var calendarData = mutableMapOf(
         0 to CalendarData(0, "응용통계학", "김도엽", "미래관 2층 32호실"),
         15 to CalendarData(15, "인공지능", "김도3", "미래관 6층 23호실")
     )
+    var testData = mutableMapOf(
+        13 to CalendarData(13, "응용통계학", "김도엽", "미래관 2층 32호실"),
+        24 to CalendarData(24, "인공지능", "김도3", "미래관 6층 23호실")
+    )
+    var cells = mutableMapOf<Int, View>()
+    lateinit var spinner: Spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -62,11 +70,117 @@ class MainActivity : AppCompatActivity() {
                 val layout = createCell(500, 300, j, i, grid)
                 val cell: View = layoutInflater.inflate(R.layout.calendar_item, layout)
                 val idx = ((i - 1) * (grid.columnCount - 1)) + (j - 1)
+                cells[idx] = cell
                 if (calendarData.containsKey(idx)) {
                     val data = calendarData[idx]
                     cell.findViewById<TextView>(R.id.calendar_item_title).text = data?.title
                     cell.findViewById<TextView>(R.id.calendar_item_location).text = data?.location
                     cell.findViewById<TextView>(R.id.calendar_item_user).text = data?.user
+                    cell.setOnClickListener {
+                        val view = layoutInflater.inflate(R.layout.dialog_calendar, null)
+                        view.findViewById<EditText>(R.id.dialog_calendar_title)
+                            .setText(calendarData[idx]?.title)
+                        view.findViewById<EditText>(R.id.dialog_calendar_location)
+                            .setText(calendarData[idx]?.location)
+                        view.findViewById<EditText>(R.id.dialog_calendar_user)
+                            .setText(calendarData[idx]?.user)
+                        val builder: AlertDialog.Builder = AlertDialog.Builder(this@MainActivity)
+                        builder
+                            .setView(view)
+                            .setPositiveButton(
+                                "수정",
+                                DialogInterface.OnClickListener { dialog, index ->
+                                    calendarData[idx]?.title =
+                                        view.findViewById<EditText>(R.id.dialog_calendar_title).text.toString()
+                                    calendarData[idx]?.location =
+                                        view.findViewById<EditText>(R.id.dialog_calendar_location).text.toString()
+                                    calendarData[idx]?.user =
+                                        view.findViewById<EditText>(R.id.dialog_calendar_user).text.toString()
+                                    refreshCell(calendarData)
+                                })
+                            .setNegativeButton("취소", DialogInterface.OnClickListener { dialog, _ ->
+                                dialog.cancel()
+                            })
+                            .create().show()
+                    }
+                    cell.setOnLongClickListener {
+                        val builder = AlertDialog.Builder(this@MainActivity)
+                        builder.setMessage("삭제하시겠습니까?")
+                            .setPositiveButton("삭제",
+                                DialogInterface.OnClickListener { dialog, id ->
+                                    calendarData.remove(idx)
+                                    refreshCell(calendarData)
+                                })
+                            .setNegativeButton("취소",
+                                DialogInterface.OnClickListener { dialog, id ->
+                                    dialog.cancel()
+                                })
+                            .create().show()
+                        return@setOnLongClickListener true
+                    }
+                } else {
+                    layout.setOnClickListener {
+                        val view = layoutInflater.inflate(R.layout.dialog_calendar, null)
+                        val builder: AlertDialog.Builder = AlertDialog.Builder(this@MainActivity)
+                        builder
+                            .setView(view)
+                            .setPositiveButton(
+                                "등록",
+                                DialogInterface.OnClickListener { dialog, index ->
+                                    calendarData[idx] = CalendarData(
+                                        idx,
+                                        view.findViewById<EditText>(R.id.dialog_calendar_title).text.toString(),
+                                        view.findViewById<EditText>(R.id.dialog_calendar_user).text.toString(),
+                                        view.findViewById<EditText>(R.id.dialog_calendar_location).text.toString()
+                                    )
+                                    refreshCell(calendarData)
+                                })
+                            .setNegativeButton("취소", DialogInterface.OnClickListener { dialog, _ ->
+                                dialog.cancel()
+                            })
+                            .create().show()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun refreshCell(datas: MutableMap<Int, CalendarData>) {
+        val grid: GridLayout = findViewById(R.id.gridLayout)
+
+        for (i: Int in 1 until grid.rowCount) {
+            for (j: Int in 1 until grid.columnCount) {
+                val idx = ((i - 1) * (grid.columnCount - 1)) + (j - 1)
+                val cell: View? = cells[idx]
+                if (datas.containsKey(idx)) {
+                    val data = datas[idx]
+                    cell?.findViewById<TextView>(R.id.calendar_item_title)?.text = data?.title
+                    cell?.findViewById<TextView>(R.id.calendar_item_location)?.text = data?.location
+                    cell?.findViewById<TextView>(R.id.calendar_item_user)?.text = data?.user
+                } else {
+                    cell?.findViewById<TextView>(R.id.calendar_item_title)?.text = ""
+                    cell?.findViewById<TextView>(R.id.calendar_item_location)?.text = ""
+                    cell?.findViewById<TextView>(R.id.calendar_item_user)?.text = ""
+                }
+            }
+        }
+    }
+
+    private fun compareData(data0: MutableMap<Int, CalendarData>, data1: MutableMap<Int, CalendarData>) {
+        val grid: GridLayout = findViewById(R.id.gridLayout)
+
+        for (i: Int in 1 until grid.rowCount) {
+            for (j: Int in 1 until grid.columnCount) {
+                val idx = ((i - 1) * (grid.columnCount - 1)) + (j - 1)
+                val cell: View? = cells[idx]
+                if(!data0.containsKey(idx) and !data1.containsKey(idx)) {
+                    cell?.findViewById<TextView>(R.id.calendar_item_title)?.text = "비는 시간"
+                    cell?.findViewById<TextView>(R.id.calendar_item_location)?.text = ""
+                    cell?.findViewById<TextView>(R.id.calendar_item_user)?.text = ""
+                } else {
+                    cell?.findViewById<TextView>(R.id.calendar_item_title)?.text = ""
+                    cell?.findViewById<TextView>(R.id.calendar_item_location)?.text = ""
+                    cell?.findViewById<TextView>(R.id.calendar_item_user)?.text = ""
                 }
             }
         }
@@ -83,5 +197,40 @@ class MainActivity : AppCompatActivity() {
         layout.layoutParams = param
         grid.addView(layout)
         return layout
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.android_action_bar_spinner_menu, menu)
+        val item: MenuItem = menu.findItem(R.id.spinner)
+        spinner = MenuItemCompat.getActionView(item) as Spinner
+        val adapter = ArrayAdapter<String>(
+            this,
+            R.layout.support_simple_spinner_dropdown_item,
+            arrayOf("시간표 1", "시간표 2", "시간표 대조")
+        )
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item)
+        spinner.adapter = adapter
+        spinner?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                if (position == 0) {
+                    refreshCell(calendarData)
+                } else if (position == 1) {
+                    refreshCell(testData)
+                } else if (position == 2) {
+                    compareData(calendarData, testData)
+                }
+            }
+
+        }
+        return true
     }
 }
